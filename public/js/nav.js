eaeaf51ef63d4708e81d434d2b3a0dd5fc337e96
sasaharad <sasaharad@egao-inc.co.jp>
2016-02-12 22:43:11 +0900

    スライドメニューにスクロール追加【失敗】

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,22 +1,22 @@
 
 $(function(){
-var menu = $('#slide_menu'), // スライドインするメニューを指定
-    menuBtn = $('#toggle-btn'), // メニューボタンを指定
-    body = $(document.body),     
-    menuWidth = menu.outerWidth();                	
+var $menu = $('#slide_menu'), // スライドインするメニューを指定
+    $menuBtn = $('#toggle-btn'), // メニューボタンを指定
+    $wrap = $('#header'),     
+    menuWidth = $menu.outerWidth();                	
      
     // メニューボタンをクリックした時の動き
-    menuBtn.on('click', function(){
-    	// body に open クラスを付与する
-    	body.toggleClass('open');
-    	    if(body.hasClass('open')){
-    	        // open クラスが body についていたらメニューをスライドインする
-    	        body.animate({'ridht' : menuWidth }, 300);            
-    	        menu.animate({'right' : 0 }, 300);                    
+    $menuBtn.on('click', function(){
+    	// $wrap に open クラスを付与する
+    	$wrap.toggleClass('open');
+    	    if($wrap.hasClass('open')){
+    	        // open クラスが $wrap についていたらメニューをスライドインする
+    	        $wrap.animate({'ridht' : menuWidth }, 300);            
+    	        $menu.animate({'right' : 0 }, 300);                    
     	    } else {
-    	        // open クラスが body についていなかったらスライドアウトする
-    	        menu.animate({'right' : -menuWidth }, 300);
-    	        body.animate({'right' : 0 }, 300);            
+    	        // open クラスが $wrap についていなかったらスライドアウトする
+    	        $menu.animate({'right' : -menuWidth }, 300);
+    	        $wrap.animate({'right' : 0 }, 300);            
     	    }             
     });
 });    
